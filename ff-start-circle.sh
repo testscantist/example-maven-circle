243836cdea15d9de9be55179f57b25c859711c2a
zengfeifan <turly221@gmail.com>
2018-05-03 12:47:23 +0800

    include project info

--- ff-start-circle.sh
+++ ff-start-circle.sh
@@ -7,17 +7,21 @@ echo "inside ff-start.sh script"
 SCANTIST_IMPORT_URL="http://e8482e5c.ngrok.io/import/"
 
 show_project_info() {
-  echo "TRAVIS_EVENT_TYPE $TRAVIS_EVENT_TYPE"
   echo "CIRCLE_BRANCH $CIRCLE_BRANCH"
   echo "CIRCLE_SHA1 $CIRCLE_SHA1"
   echo "CIRCLE_USERNAME $CIRCLE_USERNAME"
   echo "CIRCLE_REPOSITORY_URL $CIRCLE_REPOSITORY_URL"
   echo "CIRCLE_PR_REPONAME $CIRCLE_PR_REPONAME"
   echo "CI_PULL_REQUEST $CI_PULL_REQUEST"
+  echo "CIRCLE_PROJECT_USERNAME $CIRCLE_PROJECT_USERNAME"
+  echo "CIRCLE_PROJECT_REPONAME $CIRCLE_PROJECT_REPONAME"
   echo "=================project info====================="
 }
 echo "=================show_project_info================="
 show_project_info
+
+$repo_name=$CIRCLE_PROJECT_USERNAME"/"$CIRCLE_PROJECT_REPONAME
+$commit_sha=$CIRCLE_SHA1
 
 ls
 
@@ -28,7 +32,7 @@ pyenv versions
 
 pyenv global 3.6.2
 
-python TreeBuilder.py $cwd
+python TreeBuilder.py $cwd $repo_name $commit_sha
 
 #Log that the script download is complete and proceeding
 echo "Uploading report at $SCANTIST_IMPORT_URL"
